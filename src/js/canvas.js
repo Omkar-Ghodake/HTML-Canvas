1877e4317ee3c1469b2213cd9426655c3b0a19e5
test-bot-69 <testbot.1831@gmail.com>
2023-05-05 12:08:06 +0530

    Gravity

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -13,10 +13,13 @@ const mouse = {
 
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
+var gravity = 1
+const friction = 0.7
+
 // Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
+addEventListener('mousemove', (e) => {
+  mouse.x = e.clientX
+  mouse.y = e.clientY
 })
 
 addEventListener('resize', () => {
@@ -26,11 +29,17 @@ addEventListener('resize', () => {
   init()
 })
 
+addEventListener('click', () => {
+  init()
+})
+
 // Objects
-class Object {
-  constructor(x, y, radius, color) {
+class Ball {
+  constructor(x, y, dx, dy, radius, color) {
     this.x = x
     this.y = y
+    this.dx = dx
+    this.dy = dy
     this.radius = radius
     this.color = color
   }
@@ -38,23 +47,44 @@ class Object {
   draw() {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    // c.strokeStyle = 'white'
+    c.lineWidth = 2
+    c.stroke()
     c.fillStyle = this.color
     c.fill()
     c.closePath()
   }
 
   update() {
+    if (this.y + this.radius + this.dy > canvas.height) {
+      this.dy = -this.dy * friction
+    } else {
+      this.dy += gravity
+    }
+
+    if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius <= 0) {
+      this.dx = -this.dx
+    }
+
+    this.x += this.dx
+    this.y += this.dy
+
     this.draw()
   }
 }
 
 // Implementation
-let objects
+let ballArray
 function init() {
-  objects = []
+  ballArray = []
 
   for (let i = 0; i < 400; i++) {
-    // objects.push()
+    var radius = utils.randomIntFromRange(8, 20)
+    var x = utils.randomIntFromRange(radius, canvas.width - radius)
+    var y = utils.randomIntFromRange(0, canvas.height - radius)
+    var dx = utils.randomIntFromRange(-2, 2)
+    var dy = utils.randomIntFromRange(-2, 2)
+    ballArray.push(new Ball(x, y, dx, dy, radius, utils.randomColor(colors)))
   }
 }
 
@@ -63,10 +93,9 @@ function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  ballArray.forEach(ball => {
+    ball.update()
+  })
 }
 
 init()
